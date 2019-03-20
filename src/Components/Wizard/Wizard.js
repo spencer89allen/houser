6e75861aeae319f7d5717d877d1e354629d1df5a
Spencer Allen <spencerallen@Spencers-MacBook-Pro.local>
2019-03-19 20:57:35 -0600

    part 1 step 2 completed

--- src/Components/Wizard/Wizard.js
+++ src/Components/Wizard/Wizard.js
@@ -5,6 +5,20 @@ import { Link } from 'react-router-dom';
 
 class Wizard extends Component {
 
+    state = {
+        name: '',
+        address: '',
+        city: '',
+        state: '',
+        zip: '',
+    };
+
+    handleInput = (name, value) => {
+        this.setState({
+            [name]: value,
+        });
+        console.log(this.state)
+    };
 
 
 
@@ -14,31 +28,61 @@ class Wizard extends Component {
                 <div className="field">
                     <label className="label">Property Name</label>
                     <div className="control">
-                        <input className="input" type="text" placeholder="e.g The Abode"></input>
+                        <input  className="input" 
+                                type="text" 
+                                placeholder="e.g The Abode"
+                                value={this.state.name}
+                                name='name'
+                                onChange={(e) => this.handleInput(e.target.name, e.target.value)}
+                                ></input>
                     </div>
                 </div>
                 <div className="field">
                     <label className="label">Address</label>
                     <div className="control">
-                        <input className="input" type="email" placeholder="e.g. 123 Sesame Street"></input>
+                        <input  className="input" 
+                                type="text" 
+                                placeholder="e.g. 123 Sesame Street"
+                                value={this.state.address}
+                                name='address'
+                                onChange={(e) => this.handleInput(e.target.name, e.target.value)}
+                                ></input>
                     </div>
                 </div>
                 <div className="field">
                     <label className="label">City</label>
                     <div className="control">
-                        <input className="input" type="email" placeholder="e.g. Somewhere"></input>
+                        <input  className="input" 
+                                type="text" 
+                                placeholder="e.g. Somewhere"
+                                value={this.state.city}
+                                name='city'
+                                onChange={(e) => this.handleInput(e.target.name, e.target.value)}
+                                ></input>
                     </div>
                 </div>
                 <div className="field">
                     <label className="label">State</label>
                     <div className="control">
-                        <input className="input" type="email" placeholder="e.g. Somewhere Else"></input>
+                        <input  className="input" 
+                                type="text" 
+                                placeholder="e.g. Somewhere Else"
+                                value={this.state.state}
+                                name='state'
+                                onChange={(e) => this.handleInput(e.target.name, e.target.value)}
+                                ></input>
                     </div>
                 </div>
                 <div className="field">
                     <label className="label">Zip</label>
                     <div className="control">
-                        <input className="input" type="email" placeholder="e.g. 12345"></input>
+                        <input  className="input" 
+                                type="text" 
+                                placeholder="e.g. 12345"
+                                value={this.state.zip}
+                                name='zip'
+                                onChange={(e) => this.handleInput(e.target.name, e.target.value)}
+                                ></input>
                     </div>
                 </div>
                 <Link to='/' className="subtitle button is-small is-danger">
